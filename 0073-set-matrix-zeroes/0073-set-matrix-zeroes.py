1f729c56cd19183d013317b9d36c74cace39821a
Zane Brackley <100737262+ZaneBrackley@users.noreply.github.com>
2025-03-22 11:39:06 +1100

    Time: 1 ms (87.05%), Space: 13.2 MB (43.59%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        
+        n = len(matrix)
+        m = len(matrix[0])
+
+        rows = [False] * n
+        cols = [False] * m
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    rows[i] = True
+                    cols[j] = True
+
+        for i in range(n):
+            for j in range(m):
+                if rows[i] or cols[j]:
+                    matrix[i][j] = 0
