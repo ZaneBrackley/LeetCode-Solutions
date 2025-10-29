e3fc0cfd0eb20a0950f210c580fe3ac2f6659f44
Zane Brackley <100737262+ZaneBrackley@users.noreply.github.com>
2025-10-29 18:06:29 +1100

    Time: 15 ms (96.05%), Space: 16.1 MB (64.89%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            groups[key].append(word)
+        
+        return list(groups.values())
+        
