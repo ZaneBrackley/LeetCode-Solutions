67305b9f71d867dbf7790f98b0d2532e1e8d7c90
Zane Brackley <100737262+ZaneBrackley@users.noreply.github.com>
2025-11-04 20:44:46 +1100

    Time: 13 ms (40.37%), Space: 12.3 MB (81.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -6,15 +6,15 @@ class Solution(object):
         """
         if x < 0:
             return False
-        elif x < 10:
+        if x < 10:
             return True
-        else:
-            x = str(x)
-            left, right = 0, len(x) - 1
-            while left <= right:
-                if x[left] == x[right]:
-                    left += 1
-                    right -= 1
-                else:
-                    return False
-            return True
+        x = str(x)
+        n = len(x)
+        left, right = 0, n-1
+        while left <= right:
+            if x[left] == x[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
