21f1aa7ea05eaa63dd08ac3e2f06673824ed5803
Zane Brackley <100737262+ZaneBrackley@users.noreply.github.com>
2025-11-04 17:47:23 +1100

    Time: 12 ms (85.55%), Space: 26 MB (18.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,13 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums.sort()
-        n = len(nums)
-        i, j = 0, 1
-        while j < len(nums):
-            if nums[i] == nums[j]:
-                return True
-            else:
-                i += 1 
-                j += 1
-        return False
+        return False if len(nums) == len(set(nums)) else True
