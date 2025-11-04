852e7ba1631c5f63b476918c6ebd3732f7d5780b
Zane Brackley <100737262+ZaneBrackley@users.noreply.github.com>
2025-11-04 16:15:38 +1100

    Time: 2445 ms (75.97%), Space: 130.6 MB (63.12%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,21 @@
+import math
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 2:
+            return 0
+        sieve = [True] * n
+        sieve[0] = sieve[1] = False
+
+        i = 2
+        while i * i < n:
+            if sieve[i]:
+                for j in range(i*i, n, i):
+                    sieve[j] = False
+            i += 1
+
+        return sum(sieve)     
