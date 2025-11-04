2a74e4e73656d4a595a842a14c221284ffc69ede
Zane Brackley <100737262+ZaneBrackley@users.noreply.github.com>
2025-11-04 20:40:40 +1100

    Time: 115 ms (61.05%), Space: 13.6 MB (26.74%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,28 +1,34 @@
-class Solution:
-    def calculate(self, s):    
-        def calc(it):
-            def update(op, v):
-                if op == "+": stack.append(v)
-                if op == "-": stack.append(-v)
-                if op == "*": stack.append(stack.pop() * v)
-                if op == "/": stack.append(int(stack.pop() / v))
-        
-            num, stack, sign = 0, [], "+"
-            
-            while it < len(s):
-                if s[it].isdigit():
-                    num = num * 10 + int(s[it])
-                elif s[it] in "+-*/":
-                    update(sign, num)
-                    num, sign = 0, s[it]
-                elif s[it] == "(":
-                    num, j = calc(it + 1)
-                    it = j - 1
-                elif s[it] == ")":
-                    update(sign, num)
-                    return sum(stack), it + 1
-                it += 1
-            update(sign, num)
-            return sum(stack)
+class Solution(object):
+    def calculate(self, s):
+        stack = []
+        result = 0
+        sign = 1
+        num = 0
 
-        return calc(0)
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+
+            elif ch in "+-":
+                result += sign * num
+                num = 0
+                sign = 1 if ch == '+' else -1
+
+            elif ch == '(':
+                stack.append(result)
+                stack.append(sign)
+
+                result = 0
+                sign = 1
+
+            elif ch == ')':
+                result += sign * num
+                num = 0
+
+                prev_sign = stack.pop()
+                prev_result = stack.pop()
+
+                result = prev_result + prev_sign * result
+
+        result += sign * num
+        return result
